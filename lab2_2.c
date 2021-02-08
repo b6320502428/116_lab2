9bf5f68d473539cf60cea66ffa316a00db21dddf
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 16:56:09 +0700

    lab2_2 up1

--- lab2_2.c
+++ lab2_2.c
@@ -2,9 +2,11 @@
 #include <string.h>
 int main()
 {
-    float time,pride;
+    float time,pride,sec;
     char pro;
+    int min;
     scanf("%c %f",&pro,&time);
-
+    min=floor(time);
+    sec=(time-min)*100;
     return 0;
 }
