c8a7dab5cc4ad0155ec48566e084a674c0dbc541
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 16:47:24 +0700

    lab2_1 up3

--- lab2_1.c
+++ lab2_1.c
@@ -10,5 +10,18 @@ int main()
         strcpy(g, "A");
     else if(d>=75 && d<=79)
         strcpy(g, "B+");
+    else if(d>=70 && d<=74)
+        strcpy(g, "B");
+    else if(d>=65 && d<=69)
+        strcpy(g, "C+");
+    else if(d>=60 && d<=64)
+        strcpy(g, "C");
+    else if(d>=55 && d<=59)
+        strcpy(g, "D+");
+    else if(d>=50 && d<=54)
+        strcpy(g, "D");
+    else
+        strcpy(g, "F");
+
     return 0;
 }
