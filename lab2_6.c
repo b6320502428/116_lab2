f4e8c127c7dfed230c86e6d48b48a36dde49fa21
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 19:40:08 +0700

    lab2_6 up2

--- lab2_6.c
+++ lab2_6.c
@@ -2,8 +2,47 @@
 #include <string.h>
 int main()
 {
-    int num;
+    int num,n,i;
     char val[9]="";
+    scanf("%d",&num);
     sprintf(val, "%d",num);
+    n=strlen(val);
+    for(i=0; i<n; i++)
+    {
+        switch(val[i])
+        {
+        case '1':
+            printf("One");
+            break;
+        case '2':
+            printf("Two");
+            break;
+        case '3':
+            printf("Three");
+            break;
+        case '4':
+            printf("Four");
+            break;
+        case '5':
+            printf("Five");
+            break;
+        case '6':
+            printf("Six");
+            break;
+        case '7':
+            printf("Seven");
+            break;
+        case '8':
+            printf("Eight");
+            break;
+        case '9':
+            printf("Nine");
+            break;
+        default:
+            printf("Zero");
+        }
+        if(i!=n-1)
+            printf("-");
+    }
     return 0;
 }
