957495da80c26a66816b9e6a7692606975fc65fb
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 17:05:39 +0700

    lab2_2 up3

--- lab2_2.c
+++ lab2_2.c
@@ -10,10 +10,16 @@ int main()
     sec=(time-min)*100;
     if(pro=='A')
     {
-        if(min<=200)
+        if(min==200)
         {
             if(sec==0)
                 pride=199;
+            else
+                pride=199+(sec*3/60);
+        }
+        else if(min>200)
+        {
+            pride=199+(min-200)*3+(sec*3/60);
         }
     }
     return 0;
