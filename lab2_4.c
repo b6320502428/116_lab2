51f323a47ac1f2fe8e966ee02147cafa9fdd1c9a
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 18:47:12 +0700

    lab2_4 up4

--- lab2_4.c
+++ lab2_4.c
@@ -4,41 +4,46 @@ int main()
     char name,b=92;
     int day;
     scanf("%c %d",&name,&day);
-    if(name>=65 && name<=73)
+    if(name>=65&&name<=90&&day>0&&day<32)
     {
-        if(day%2==1)
+        if(name>=65 && name<=73)
         {
-            if(day%10==5)
-                printf("%c(^_^)/",b);
+            if(day%2==1)
+            {
+                if(day%10==5)
+                    printf("%c(^_^)/",b);
+                else
+                    printf("(^_^)");
+            }
             else
-                printf("(^_^)");
+                printf("{@_@}");
         }
-        else
-        printf("{@_@}");
-    }
-    else if(name>=74 && name<=82)
-    {
-        if(day%2==1)
+        else if(name>=74 && name<=82)
         {
-            if(day%10==5)
-                printf("%c(*o*)/",b);
+            if(day%2==1)
+            {
+                if(day%10==5)
+                    printf("%c(*o*)/",b);
+                else
+                    printf("(*o*)");
+            }
             else
-                printf("(*o*)");
+                printf("{*v*}");
         }
-        else
-        printf("{*v*}");
-    }
-    else if(name>=83 && name<=90)
-    {
-        if(day%2==1)
+        else if(name>=83 && name<=90)
         {
-            if(day%10==5)
-                printf("%c(T_T)/",b);
+            if(day%2==1)
+            {
+                if(day%10==5)
+                    printf("%c(T_T)/",b);
+                else
+                    printf("(T_T)");
+            }
             else
-                printf("(T_T)");
+                printf("{x_x}");
         }
-        else
-        printf("{x_x}");
     }
+    else
+        printf("Error");
     return 0;
 }
