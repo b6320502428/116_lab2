e63d047be5c7f79973fa1dfa71d56a2c2870731c
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 17:27:27 +0700

    lab2_3 up1

--- lab2_3.c
+++ lab2_3.c
@@ -3,8 +3,20 @@
 int main()
 {
     char str[4];
-    int n[3],i;
+    int n[3],i,j,a;
     scanf("%d %d %d",&n[0],&n[1],&n[3]);
     gets(str);
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<3-i; j++)
+        {
+            if(n[j]>n[j+1])
+            {
+                a=n[j];
+                n[j]=n[j+1];
+                n[j+1]=a;
+            }
+        }
+    }
     return 0;
 }
