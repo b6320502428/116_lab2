5fbef0fd95a29255d9554fe1274ea82a0290f987
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 17:06:49 +0700

    lab2_2 up4

--- lab2_2.c
+++ lab2_2.c
@@ -18,9 +18,9 @@ int main()
                 pride=199+(sec*3/60);
         }
         else if(min>200)
-        {
             pride=199+(min-200)*3+(sec*3/60);
-        }
+        else
+            pride=199;
     }
     return 0;
 }
