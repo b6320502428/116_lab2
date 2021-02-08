af1058ac67d830601ccdaa2b6c9f0bf161850b60
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 19:59:21 +0700

    lab2_5 up3

--- lab2_5.c
+++ lab2_5.c
@@ -3,52 +3,48 @@ int main()
 {
     int day,month,ans;
     scanf("%d %d",&day,&month);
-    if(day<=1&&day<=7)
+    switch(month)
     {
-        switch(month)
-        {
-        case 1:
-            ans=(day+31)%7;
-            break;
-        case 2:
-            ans=(day+28)%7;
-            break;
-        case 3:
-            ans=(day+31)%7;
-            break;
-        case 4:
-            ans=(day+30)%7;
-            break;
-        case 5:
-            ans=(day+31)%7;
-            break;
-        case 6:
-            ans=(day+30)%7;
-            break;
-        case 7:
-            ans=(day+31)%7;
-            break;
-        case 8:
-            ans=(day+31)%7;
-            break;
-        case 9:
-            ans=(day+30)%7;
-            break;
-        case 10:
-            ans=(day+31)%7;
-            break;
-        case 11:
-            ans=(day+30)%7;
-            break;
-        case 12:
-            ans=(day+31)%7;
-            break;
-        default:
-            printf("Error");
-        }
-        printf("%d",ans);
-    }
-    else
+    case 1:
+        ans=(day+31)%7;
+        break;
+    case 2:
+        ans=(day+28)%7;
+        break;
+    case 3:
+        ans=(day+31)%7;
+        break;
+    case 4:
+        ans=(day+30)%7;
+        break;
+    case 5:
+        ans=(day+31)%7;
+        break;
+    case 6:
+        ans=(day+30)%7;
+        break;
+    case 7:
+        ans=(day+31)%7;
+        break;
+    case 8:
+        ans=(day+31)%7;
+        break;
+    case 9:
+        ans=(day+30)%7;
+        break;
+    case 10:
+        ans=(day+31)%7;
+        break;
+    case 11:
+        ans=(day+30)%7;
+        break;
+    case 12:
+        ans=(day+31)%7;
+        break;
+    default:
         printf("Error");
+    }
+    printf("%d",ans);
+
     return 0;
 }
