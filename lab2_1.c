84f7d3f8a05aae74c3e5b423fba834189e0c40bf
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 16:44:18 +0700

    lab2_1 up2

--- lab2_1.c
+++ lab2_1.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-    int a,b,c;
+    int a,b,c,d;
+    char g[4];
     scanf("%d %d %d",&a,&b,&c);
+    d=a+b+c;
+    if(d>=80 && d<=100)
+        strcpy(g, "A");
+    else if(d>=75 && d<=79)
+        strcpy(g, "B+");
     return 0;
 }
