a70516d17dbfe76206eeb1081c015cc34bb69162
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 18:33:48 +0700

    lab2_4 up1

--- lab2_4.c
+++ lab2_4.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    char name;
+    char name,b=92;
     int day;
     scanf("%c %d",&name,&day);
+    if(name>=65 && name<=73)
+    {
+        if(day%2==1)
+        {
+            if(day%10==5)
+                printf("%c(^_^)/",b);
+            else
+                printf("(^_^)",b);
+        }
+    }
     return 0;
 }
