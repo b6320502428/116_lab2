32e9a7a7c01f3347776634d02cafef95fa5b424f
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 17:40:33 +0700

    lab2_2 up8

--- lab2_2.c
+++ lab2_2.c
@@ -13,31 +13,49 @@ int main()
         if(min==200)
         {
             if(sec==0)
+            {
                 pride=199;
+            }
             else
+            {
                 pride=199+(sec*3/60);
+            }
         }
         else if(min>200)
+        {
             pride=199+(min-200)*3+(sec*3/60);
+        }
         else
+        {
             pride=199;
+        }
     }
     else if(pro=='B'||pro=='b')
     {
         if(min==400)
         {
             if(sec==0)
+            {
                 pride=299;
+            }
             else
+            {
                 pride=299+(sec*2/60);
+            }
         }
         else if(min>400)
+        {
             pride=299+(min-400)*2+(sec*2/60);
+        }
         else
+        {
             pride=299;
+        }
     }
     else
+    {
         pride=0;
+    }
     printf("%.2f",pride);
     return 0;
 }
