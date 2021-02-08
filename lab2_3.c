99fcc1eff14c441ce442601afa302624156b365d
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-08 17:42:38 +0700

    lab2_3 up2

--- lab2_3.c
+++ lab2_3.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 int main()
 {
     char str[4];
     int n[3],i,j,a;
-    scanf("%d %d %d",&n[0],&n[1],&n[3]);
     gets(str);
+    scanf("%d %d %d",&n[0],&n[1],&n[2]);
     for(i=0; i<n; i++)
     {
         for(j=0; j<3-i; j++)
@@ -18,5 +18,11 @@ int main()
             }
         }
     }
+    if(!(strcmp(str,"ABC")))
+        printf("%d %d %d",n[0],n[1],n[2]);
+    if(!(strcmp(str,"ACB")))
+        printf("%d %d %d",n[0],n[2],n[1]);
+    if(!(strcmp(str,"BAC")))
+        printf("%d %d %d",n[1],n[0],n[2]);
     return 0;
 }
